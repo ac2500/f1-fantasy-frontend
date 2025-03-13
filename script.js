85b51d49372b48eca65f6fc78f45066bcc54eb0a
Austin Carr <austincarr@Austins-MacBook-Air-2.local>
2025-03-13 19:19:26 -0400

    Initial frontend setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const backendUrl = "https://f1-fantasy-backend-mddo.onrender.com";
+
+function fetchTeams() {
+    fetch(`${backendUrl}/teams`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("teams").innerText = JSON.stringify(data, null, 2);
+        })
+        .catch(error => console.error("Error fetching teams:", error));
+}
